ad2083ee97b1d049be7249547b2ceee866fcdbb2
Samuele de Tomasi <el3um4s@gmail.com>
2022-07-27 15:27:22 +0200

    Update query.test.ts

--- src/__tests__/query.test.ts
+++ src/__tests__/query.test.ts
@@ -2,6 +2,7 @@ import path from "path";
 import * as fs from "fs";
 
 import { query } from "../index";
+import { table } from "../index";
 
 const database = "./src/__tests__/test.mdb";
 
@@ -137,5 +138,76 @@ WHERE (userName Like '%1°%');`;
     });
 
     expect(result).toBe(true);
+  });
+
+  test("query.sql - INSERT", async () => {
+    const rowsBeforeInsert = await table.count({ database, table: "Users" });
+    const sql = `INSERT INTO [Users](userName, userAge, mail) VALUES ("Topo Gigio", 6, "topogigio@test.com");`;
+    await query.sql({
+      database,
+      sql,
+    });
+
+    const rowsAfterInsert = await table.count({ database, table: "Users" });
+
+    expect(rowsAfterInsert).toBe(rowsBeforeInsert + 1);
+  });
+
+  test("query.sql - UPDATE", async () => {
+    const rowsBeforeUpdate = await table.count({ database, table: "Users" });
+
+    // UPDATE table_name
+    // SET column1 = value1, column2 = value2, ...
+    // WHERE condition;
+    const originalValue = await query.sql({
+      database,
+      sql: `SELECT * FROM Users WHERE userName like ('%Topo%');`,
+    });
+
+    expect(originalValue[0]).toEqual({
+      userName: "Topo Gigio",
+      userAge: "6",
+      mail: "topogigio@test.com",
+    });
+
+    const sql = `
+    UPDATE [Users]
+    SET userAge = 100, mail = "oldTopo@test.com"
+    WHERE userName = "Topo Gigio";
+    `;
+    await query.sql({
+      database,
+      sql,
+    });
+
+    const rowsAfterUpdate = await table.count({ database, table: "Users" });
+
+    expect(rowsAfterUpdate).toBe(rowsBeforeUpdate);
+
+    const updatedValue = await query.sql({
+      database,
+      sql: `SELECT * FROM Users WHERE userName like ('%Topo%');`,
+    });
+
+    expect(updatedValue[0]).toEqual({
+      userName: "Topo Gigio",
+      userAge: "100",
+      mail: "oldTopo@test.com",
+    });
+  });
+
+  test("query.sql - DELETE", async () => {
+    const rowsBeforeDelete = await table.count({ database, table: "Users" });
+    const sql = `
+    DELETE FROM [Users]
+    WHERE userName = "Topo Gigio";`;
+    await query.sql({
+      database,
+      sql,
+    });
+
+    const rowsAfterDelete = await table.count({ database, table: "Users" });
+
+    expect(rowsAfterDelete).toBe(rowsBeforeDelete - 1);
   });
 });
